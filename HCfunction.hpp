a2cca7895646a7bea8a305112b7d6a57929ade53
TzuChieh <james093131@gmail.com>
2020-09-17 02:46:26 +0800

    Global_Update

--- HCfunction.hpp
+++ HCfunction.hpp
@@ -1,4 +1,57 @@
 #include "normal.hpp"
-void Enumnext(char *,int );
-void NeighborSelection(char *,int );
 
+void Enumnext(vector<char> &arr,int len){
+	int i=len-1;
+	int ran=rand()%2;
+	if (ran==1)
+	{
+		add(arr,len);
+	}
+	else{
+		sub(arr,len);
+	}
+	
+}
+void NeighborSelection(vector<char> &arr,int size){
+	int ran=rand()%size;
+	if(arr[ran]=='1')
+		arr[ran]='0';
+	else
+		arr[ran]='1';
+    
+}
+void ITER(int iteration,int len,string method)
+{
+	onemax.resize(len);
+	create(onemax,len);
+	opt=Evaluate(onemax,len);
+	int k=0;
+	while(k<iteration)
+		{
+			vector <char>  temp;
+			temp.resize(len);
+			temp.assign(onemax.begin(),onemax.end());
+			if(method == std::string("r"))
+				NeighborSelection(temp,len);
+			else 
+				Enumnext(temp,len);
+			int curropt=Evaluate(temp,len);
+			compare(onemax,temp,len,opt,curropt);
+			cout<<"iteration"<<k+1<<":"<<opt<<endl;
+			if(opt==len)
+				break;			
+			k++;
+		}
+}
+void RUN(int run,int len ,string method,int iteration)
+{
+	int r=0;
+	while(r<run){
+		ITER(iteration,len,method);
+		runresult[r]=opt;
+		r++;
+	}
+	avgiteration=avg(runresult,r);
+	cout<<"Average Optima :"<<avgiteration<<endl;
+	Read_File(iteration,avgiteration,method);
+}
--- normal.hpp
+++ normal.hpp
@@ -4,12 +4,113 @@
 #include<sstream>
 #include<stdlib.h>
 #include <string.h>
+#include <vector>
 #include<time.h>
 using namespace std;
-void create(char *,int );
-void Print(char *,int );
-int Evaluate(char *,int );
-void add(char *,int );
-void sub (char *,int );
-int avg(int *,int );
-void compare(char *,char *,int ,int &,int );
+
+
+vector<int> runresult;
+vector <char>  onemax;
+int opt;
+int avgiteration;
+
+
+void create(vector<char> &arr,int size)
+{
+	for(int i=0;i<size;i++)
+	{
+  	int a=rand()%2;
+    char c =a+'0';
+    arr[i]=c;
+    }
+}
+ void Print(vector<char> &arr,int size){
+	for(int i=0;i<size;i++)
+	{
+		cout<<arr[i]<<' ';
+	}
+	cout<<endl;
+} 
+int Evaluate(vector<char> &arr,int size)
+{
+	int optimal=0;
+	for(int i=0;i<size;i++)
+	{
+		if(arr[i]=='1')
+			optimal++;
+	}
+	return optimal;
+}
+void add(vector<char> &arr,int len)
+{
+	int i=len-1;
+	int chc=1;
+	while(chc){
+    	if(arr[i]=='1')
+		{
+			arr[i]='0';
+			i--;
+		}
+		else if (arr[i]=='0')
+		{
+			arr[i]='1';
+			chc=0;
+			break;
+		}
+	} 
+}
+void sub (vector<char> &arr,int len)
+{
+	int i=len-1;
+	int chc=1;
+	while(chc){
+    	if(arr[i]=='1')
+		{
+			arr[i]='0';
+			chc=0;
+			break;
+		}
+		else if (arr[i]=='0')
+		{
+			arr[i]='1';
+			i--;
+		}
+	} 
+}
+int avg(vector<int> arr,int len)
+{
+	int sum=0;
+	for(int i=0;i<len;i++)
+	{
+		sum+=arr[i];
+		cout<<"Run"<<i+1<<':'<<arr[i]<<endl;
+	}
+	int avg=sum/len;
+	return avg;
+}
+void compare(vector<char> &arr,vector<char> &temp,int len,int &opt,int curropt)
+{
+	if(curropt>=opt)
+	{
+		for(int i=0;i<len;i++)
+		{
+			arr[i]=temp[i];
+		}
+		opt=curropt;
+	}
+
+	
+}
+void Read_File(int iteration,int avgiteration,string method)
+{
+	fstream file;//寫檔
+	file.open("Result.txt",ios::app);
+	file<<"Iteration :"<<iteration<<' '<<"Optima :"<<avgiteration<<' ';
+	if(method == std::string("r"))
+	{
+		file<<"Using Function :"<<"random"<<endl;
+	}
+	else
+		file<<"Using Function :"<<"leftright"<<endl;
+}
+
